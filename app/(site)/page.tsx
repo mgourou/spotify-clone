c768a77d63277960360e3a441d50be83cc021cff
mgourou <mohamedgourou@outlook.fr>
2023-11-19 03:22:49 +0100

    sidebar/main-content

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import Header from '../../components/Header'
+
+export default function Home() {
+  return (
+    <div className='bg-neutral-900 rounded-lgh-full w-full overflow-hidden overflow-y-auto'>
+      <Header>
+
+      </Header>
+    </div>
+  )
+}
