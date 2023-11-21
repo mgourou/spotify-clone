0d1b9649eea6b4f85455aa724d8952d779442e70
mgourou <mohamedgourou@outlook.fr>
2023-11-21 01:44:19 +0100

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Sidebar from '@/components/Sidebar'
 import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import './globals.css'
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,12 @@ import { useRouter } from 'next/navigation';
 import { twMerge } from 'tailwind-merge'
 import { RxCaretLeft , RxCaretRight } from 'react-icons/rx'
 import { HiHome } from 'react-icons/hi'
+import { FaUserAlt } from 'react-icons/fa'
 import { BiSearch } from 'react-icons/bi'
 import { withRouter } from 'next/router';
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useUser } from '@/hooks/useUser';
+import { toast } from 'react-hot-toast';
 
 import useAuthModal from '@/hooks/useAuthModal';
 
@@ -16,13 +20,26 @@ interface HeaderProps {
     className?: string;
 }
 
-const Header:React.FC<HeaderProps> = ({children,className}) => {
+const Header:React.FC<HeaderProps> = ({
+    children,
+    className
+}) => {
 
     const authModal = useAuthModal();
-
     const router = useRouter();
-    const handleLogout = () => {
-    
+
+    const supabaseClient = useSupabaseClient();
+    const { user } = useUser();
+
+    const handleLogout = async () => {
+            const { error } = await supabaseClient.auth.signOut();
+            router.refresh();
+
+            if (error) {
+                toast.error(error.message);
+            } else {
+                toast.success("Logged out!")
+            }
     }
 
     return (
@@ -49,18 +66,29 @@ const Header:React.FC<HeaderProps> = ({children,className}) => {
                     </button>
                 </div>
                 <div className='flex justify-between items-center gap-x-4'>
-                    <>
-                        <div>
-                            <Button onClick={authModal.onOpen} className=' bg-transparent text-neutral-300 font-medium'>
-                                Sign up
+                    {user ? (
+                        <div className='flex gap-x-4 items-center'> 
+                            <Button onClick={handleLogout} className='bg-white px-6 py-2'>
+                                Logout
+                            </Button>
+                            <Button onClick={() => router.push('/account')}  className='bg-white'>
+                                <FaUserAlt />
                             </Button>
                         </div>
-                        <div>
-                            <Button onClick={authModal.onOpen} className=' bg-white px-6 py-2'>
-                               Log in
-                            </Button>
-                        </div>
-                    </>
+                    ) : (
+                        <>
+                            <div>
+                                <Button onClick={authModal.onOpen} className=' bg-transparent text-neutral-300 font-medium'>
+                                    Sign up
+                                </Button>
+                            </div>
+                            <div>
+                                <Button onClick={authModal.onOpen} className=' bg-white px-6 py-2'>
+                                Log in
+                                </Button>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
             {children}
